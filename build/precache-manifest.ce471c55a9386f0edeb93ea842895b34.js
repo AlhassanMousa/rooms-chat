93939983e7f0c9e558e8ab2bbac0b68dcad62b82
Alhassan Mousa <AlhassanMousa97@gmail.com>
2022-07-30 20:17:30 +0200

    Add files via upload

---
+++ build/precache-manifest.ce471c55a9386f0edeb93ea842895b34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9ce00f888e4db81f2bed4195ed47eef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "457881b7a5c7379c866c",
+    "url": "/static/css/main.4b36c359.chunk.css"
+  },
+  {
+    "revision": "a71884c88b017ee6bdbe",
+    "url": "/static/js/2.54415c87.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.54415c87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "457881b7a5c7379c866c",
+    "url": "/static/js/main.e7bf22fd.chunk.js"
+  },
+  {
+    "revision": "9bc2349bfbe76e87395f",
+    "url": "/static/js/runtime-main.e5afff51.js"
+  }
+]);
